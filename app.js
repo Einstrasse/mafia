19b105af7d12fdc7b45b8a07c26ac6e149445596
Einstrasse <hg9587@naver.com>
2017-02-25 12:30:02 +0000

    session create

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ global.__path = __dirname + '/';
 var express = require('express')
   , http = require('http')
   , mongoose = require('mongoose')
+  , bodyParser = require('body-parser')
   , fs = require('fs')
-  , path = require('path');
+  , path = require('path')
+  , session = require('express-session')
+  , cookieParser = require('cookie-parser');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -18,23 +21,25 @@ Object.keys(global_json).map(function(key) {
 	global[key] = global_json[key];
 });
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.configure(function(){
-  app.set('port', port);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+// app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('MagicStrinG'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ 
+	secret: 'EineKleineNachtMusik',
+	resave: true, 
+	saveUninitialized: true,
+	cookie: { 
+		maxAge: 1000 * 60 * 60 * 12, //12시간
+		secure: false
+	} 
+}));
+app.set('port', port);
 
 mongoose.connect(__dbHost);
 
@@ -52,6 +57,8 @@ app.get('/login', routes_view.login);
 app.get('/register', routes_view.register);
 
 app.post('/ajax/register', routes_ajax.register);
+app.post('/ajax/login', routes_ajax.login);
+app.get('/ajax/sessChk', routes_ajax.sessChk);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
